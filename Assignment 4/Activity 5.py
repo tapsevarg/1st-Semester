7088b442a75e35a5716ba9622c76bad033f0df5e
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-09-12 18:04:21 -0500

    Revisions made to code
    Added space between input, processing, and output

--- Assignment 4/Activity 5.py
+++ Assignment 4/Activity 5.py
@@ -4,6 +4,7 @@ print("Enter length of room measured in feet")
 length = float(input())
 print("Enter width of room measured in feet")
 width = float(input())
+
 square_yards = (length) * (width) / 9
 
 print("You need to order " + str(square_yards) + " square yards of flooring.")
