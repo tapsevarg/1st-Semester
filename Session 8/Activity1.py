00638328cb17d56945ef915bc1c0103fdb50a5e6
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-10-06 19:23:49 -0500

    Added activity 1 python file

---
+++ Session 8/Activity1.py
@@ -0,0 +1,30 @@
+# This is an interactive program for multiplication tables.
+
+
+def get_value():
+    print("Enter value to multiply")
+    value = int(input())
+    return value
+
+
+def get_expressions():
+    print("Choose number of expressions")
+    expressions = int(input())
+    return expressions
+
+
+def process_math(value, expressions):
+    count = 1
+    while count <= expressions:
+        result = value * count
+        print(str(value) + " x " + str(count) + " = " + str(result))
+        count += 1
+
+
+def main():
+    value = get_value()
+    expressions = get_expressions()
+    process_math(value, expressions)
+
+
+main()
