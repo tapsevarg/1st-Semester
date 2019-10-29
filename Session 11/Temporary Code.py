865cea87e3c0d51c62c44cb4b9617ab539cbeac4
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-10-29 03:31:04 -0500

    Just saving code in midst of work

---
+++ Session 11/Temporary Code.py
@@ -0,0 +1,16 @@
+# This is an automatic Monty Hall problem sorted
+
+import random
+goat = 0
+car = 1
+
+
+def build_doors():
+    actual_doors = [0, 1, 0]
+    random.shuffle(actual_doors)
+    random.choice(actual_doors)
+
+
+
+
+build_doors()
