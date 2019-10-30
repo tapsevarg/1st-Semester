adca180e2d29ab65d0778b9ec4fb09c8b838cb49
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-10-29 19:39:18 -0500

    Update Temporary Code.py

--- Session 11/Temporary Code.py
+++ Session 11/Temporary Code.py
@@ -1,29 +1,64 @@
-# This is an automatic Monty Hall problem solver
+# This is an automated Monty Hall problem solver
+
 
 import random
-goat = 0
-car = 1
 
 
 def build_doors():
-    doors = [0, 1, 0]
-    random.shuffle(doors)
+    doors = ["door 1", "door 2", "door 3"]
+    print(doors)
     return doors
+    
+    
+def hide_prize(doors):
+    prize = random.choice(doors)
+    print(prize)
+    return prize
 
 
 def generate_choice(doors):
     choice = random.choice(doors)
+    print(choice)
     return choice
 
 
-def process_game(doors, choice):
-    scorecard[0, 2]
-    if choice == index.doors:
-        winner +=1
-    else:
-        loser +=1
+def test_samples1():
+    winners1 = 0
+    for count1 in range(0, 100, 1):
+        doors = build_doors()
+        prize = hide_prize(doors)
+        choice = generate_choice(doors)
+        if prize is choice:
+            winners1 += 1
+    return winners1
 
-        
-doors = build_doors()
-choice = generate_choice(doors)
-scorecard = process_game(doors, choice)
+
+def implement_switch(prize, choice, doors):
+    if not prize or choice:
+        del [doors]
+    
+
+def test_samples2():
+    winners2 = 0
+    for count2 in range(0, 100, 1):
+        doors = build_doors()
+        prize = hide_prize(doors)
+        choice = generate_choice(doors)
+        switch = implement_switch(prize, choice, doors)
+        if prize is switch:
+            winners2 += 1
+    return winners2
+
+def ouput_results(winners1, winners2):
+    print("The computer initially picked the correct door:")
+    print(winners1, " times.")
+    print("By switching, the computer picked the correct door:")
+    print(winners2)
+
+
+def main():
+    test_samples1()
+    test_samples2()
+    output_result(winners1, winners2)
+
+main()
