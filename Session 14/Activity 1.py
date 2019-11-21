f896b9ed79232f940889196ffa75b42ec0dc8036
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-11-21 15:21:35 -0600

    Updated with completed code

--- Session 14/Activity 1.py
+++ Session 14/Activity 1.py
@@ -1,5 +1,7 @@
-#This is a program to prcoess test scores from a file
+# This is a program to prcoess test scores from a file
 
+import os.path
+from os import path
 import sys
 
 
@@ -14,35 +16,55 @@ def input_file():
     except:
         print("Error reading scores.txt")
         print(sys.exc_info()[1])
+    file.close()
     return grades
 
 
 def process_file(grades):
     numbers = []
     for each in grades:
-        numbers.append(each[1])
+        each = each.split(",")
+        numbers.append(int(each[1]))
     return numbers
 
 
 def process_high(numbers):
+    maximum = max(numbers)
+    return maximum
 
 
 def process_low(numbers):
+    minimum = min(numbers)
+    return minimum
 
 
 def process_average(numbers):
+    middle = sum(numbers) / len(numbers)
+    middle = round(middle, 2)
+    return middle
 
 
-def output_scores(numbers, best, worst, middlest)
+def output_scores(grades, maximum, minimum, middle):
+    print("The highest score was " + str(maximum) + ".")
+    print("The lowest score was " + str(minimum) + ".")
+    print("The class average was " + str(middle) + ".")
+    print("The class scores were as follows:")
+    for index in grades:
+        index = index.strip()
+        index = index.replace(",", " ")
+        print(index)
 
 
 def main():
-    grades = input_file()
-    numbers = process_file(grades)
-    best = process_high(numbers)
-    worst = process_low(numbers)
-    middlest = process_average(numbers)
-    output_scores(numbers, best, worst, middlest
+    if path.exists("scores.txt"):
+        grades = input_file()
+        numbers = process_file(grades)
+        maximum = process_high(numbers)
+        minimum = process_low(numbers)
+        middle = process_average(numbers)
+        output_scores(grades, maximum, minimum, middle)
+    else:
+        print("Error finding scores.txt file")
 
 
 main()
