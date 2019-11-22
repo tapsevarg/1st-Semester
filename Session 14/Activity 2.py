420693fdb8c102b9abaaebeb1a511004886f7672
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-11-21 20:56:38 -0600

    Temp code

---
+++ Session 14/Activity 2.py
@@ -0,0 +1,68 @@
+# This is a program to process test scores from a file
+
+import os.path
+from os import path
+import sys
+
+
+def input_file():
+    grades = []
+    try:
+        with open("scores.txt", "r") as file:
+            next(file)
+            for line in file:
+                line = line.strip()
+                grades.append(line)
+    except:
+        print("Error reading scores.txt")
+        print(sys.exc_info()[1])
+    for index in grades:
+        index = index.strip()
+        index = index.replace(",", " ")
+        print(index)
+    return grades
+
+
+def process_file(grades):
+    numbers = []
+    for each in grades:
+        each = each.split(",")
+        numbers.append(int(each[1]))
+    return numbers
+
+
+def process_high(numbers):
+    maximum = max(numbers)
+    return maximum
+
+
+def process_low(numbers):
+    minimum = min(numbers)
+    return minimum
+
+
+def process_average(numbers):
+    middle = sum(numbers) / len(numbers)
+    middle = round(middle, 2)
+    return middle
+
+
+def output_scores(grades, maximum, minimum, middle):
+    print("The highest score was " + str(maximum) + ".")
+    print("The lowest score was " + str(minimum) + ".")
+    print("The class average was " + str(middle) + ".")
+
+
+def main():
+    if path.exists("scores.txt"):
+        grades = input_file()
+        numbers = process_file(grades)
+        maximum = process_high(numbers)
+        minimum = process_low(numbers)
+        middle = process_average(numbers)
+        output_scores(grades, maximum, minimum, middle)
+    else:
+        print("Error finding scores.txt file")
+
+
+main()
