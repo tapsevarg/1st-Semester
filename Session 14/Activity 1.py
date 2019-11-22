20bcf7fd11c9372354da0851d233e08086131546
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-11-21 20:46:28 -0600

    Update Activity 1.py

--- Session 14/Activity 1.py
+++ Session 14/Activity 1.py
@@ -7,15 +7,11 @@ import sys
 
 def input_file():
     grades = []
-    try:
-        with open("scores.txt", "r") as file:
+    with open("scores.txt", "r") as file:
             next(file)
             for line in file:
                 line = line.strip()
                 grades.append(line)
-    except:
-        print("Error reading scores.txt")
-        print(sys.exc_info()[1])
     return grades
 
 
