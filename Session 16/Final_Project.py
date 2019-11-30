a68d6017bd9c7b433a17c3bd93515d7568601bb5
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-11-30 17:47:23 -0600

    Update Final_Project.py

--- Session 16/Final_Project.py
+++ Session 16/Final_Project.py
@@ -1,15 +1,28 @@
 # This is a program that parses XML, processes, and formats it
 
 
-import xml.etree.cElementTree as ET
-import sys
+import urllib.request
+import xml.etree.ElementTree
 
 
 def input_xml():
-    test = ET.parse('https://www.w3schools.com/xml/plant_catalog.xml')
-    data = test.getroot()
-    print(data.tag)
-    for line in data:
-        print(data.find('name').text)
+    commons = []
+    botanicals = []
+    zones = []
+    lights = []
+    prices = []
+    availabilities = []
+    url = "https://www.w3schools.com/xml/plant_catalog.xml"
+    try:
+        page = urllib.request.urlopen(url).read()
+        page = page.decode("UTF-8")
+    except Exception as exception:
+        print(str(exception) + " reading " + url)
+        exit(1)
+    info = xml.etree.ElementTree.fromstring(page)
+    data = xml.etree.ElementTree.ElementTree(info)
+    for element in data.iter():
+        print("%s: %s" % (element.tag, element.text))
+ 
 
 input_xml()
