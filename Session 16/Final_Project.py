0cdfa33f963e930c72eab3a791b765901c614155
tapsevarg <54287999+tapsevarg@users.noreply.github.com>
2019-12-02 19:44:35 -0600

    Update Final_Project.py

--- Session 16/Final_Project.py
+++ Session 16/Final_Project.py
@@ -6,14 +6,14 @@ import xml.etree.ElementTree
 
 
 def input_xml():
-    source = {'common': [], 'botanical': [], 'zone': [],
-              'light': [], 'price': [], 'availability': []}
+    source = {'common': [], 'botanical': [], 'zone': [], 'light': [], 'price': []}
     url = "https://www.w3schools.com/xml/plant_catalog.xml"
     try:
         page = urllib.request.urlopen(url).read()
         page = page.decode("UTF-8")
     except Exception as exception:
-        print(str(exception) + " reading " + url)
+        print("Error reading " + url)
+        print(str(exception))
         exit(1)
     info = xml.etree.ElementTree.fromstring(page)
     data = xml.etree.ElementTree.ElementTree(info)
@@ -27,15 +27,25 @@ def input_xml():
         source['light'].append(child.text)
     for child in data.iter("PRICE"):
         source['price'].append(child.text)
-    for child in data.iter("AVAILABILITY"):
-        source['availability'].append(child.text)
     return source
  
 
-def test(source):
-    key_to_value_lengths = {k:len(v) for k, v in source.items()}
-    print(key_to_value_lengths)
+def test_xml(source):
+    test = [len(v) for v in source.values()]
+    if(len(set(test))==1):
+        total = test[0]
+        return total
+    else:
+        print("Error with values extracted from XML")
+        exit(1)
+
+
+def output_xml(source, total):
+    for key, value in source.items():
+        print(key, value)
+    print(total)
 
 
 source = input_xml()
-test(source)
+total = test_xml(source)
+output_xml(source, total)
